15a74ec5bd6459ba1b606d8c53ed861a3a9d4cfd
Gabriel Kabbe <gab50000@users.noreply.github.com>
2015-04-27 00:49:29 +0200

    Update resize_images.py
    Added #

--- image_resizer/resize_images.py
+++ image_resizer/resize_images.py
@@ -6,10 +6,10 @@ import ipdb
 import subprocess
 
 #Purpose of this Plugin:
-Find the following tags in articles:
-    {%mini /path/to/img %}  -> shrink these images to 800x600
-    {%mini->large /path/to/img %} -> shrink these images as before, but make a link to the original size img
-    {%slide /path/to/img1 ... /path/to/imgn %} -> make a slideshow out of these images. combinable with mini!
+#Find the following tags in articles:
+#    {%mini /path/to/img %}  -> shrink these images to 800x600
+#    {%mini->large /path/to/img %} -> shrink these images as before, but make a link to the original size img
+#    {%slide /path/to/img1 ... /path/to/imgn %} -> make a slideshow out of these images. combinable with mini!
 
 def test(instance):
     if instance._content is not None:
